22462bcd6c7cc4c1ff3e0b55d618bbdfb14a0829
nhuyp <ypham87@gmail.com>
2017-05-20 18:04:16 -0700

    Set up for txt and js files

---
+++ keys.js
@@ -0,0 +1,10 @@
+var Twitter = require('twitter');
+
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '4svzonNqed6KJ6Hm8ztKwgun1',
+  consumer_secret: 'Muzz9HVCh7oPAcbpxOCPoFO3JtztNsqWPJI2vZ7UMKDPLQaEQT',
+  access_token_key: '866087729028673536-AGzm1BGJdzAvi12pEzIM1YMTYfZvCDS',
+  access_token_secret: 'cMdjx22km3lvfbuZqyntw2N2czTHZNfwRfpfYXLrzRqGo',
+}
---
+++ liri.js
@@ -0,0 +1,18 @@
+// Grab data from keys.js
+    var key = require("./keys.js"); 
+// Store twitter keys 
+    var twitterInfo = key.twitterKeys; 
+
+// Show last 20 tweets and when they were created in git bash/terminal using my-tweets command
+
+
+    
+// Show artists, song's name, preview link from Spotify and the album that the song is from using spotify-this-sing '<song name here>' 
+
+    // if no song is provided, then program will default to "The Sign" by Ace of Base
+
+// Show movie title, movie year, IMDB rating of movie, country of origin, movie language, plot, actors, and Rotten Tomatoes URL using movie-this '<movie name here' command 
+
+    // If no movie is entered, the program will output data for the movie "Mr. Nobody"
+
+// Use fs package in Node to take text inside of random.txt to call spotify-this-song for "I Want it That Way"
