655d07c54971b55efa9cc17a802a745173c1399e
nhuyp <ypham87@gmail.com>
2017-05-23 21:14:53 -0700

    Spotify-this-song console logging

--- liri.js
+++ liri.js
@@ -1,6 +1,7 @@
 // Require spotify and OMDB
     var spotify = require('spotify');
     var request = require('request');
+    var Twitter = require('twitter'); 
 // Grab data from keys.js
     var key = require("./keys.js"); 
 // Store twitter keys 
@@ -10,39 +11,57 @@
    if (process.argv[2] == "my-tweets") {
 
         var myTweets = function () {
-        var client = new Twitter(key.twitterKeys);
+            var client = new Twitter(key.twitterKeys);
 
-        var params = {screen_name: 'nhuyp87_hw', count: 20};
-        client.get('statuses/user_timeline', params, function(error, tweets, response) {
-        
-            var data = []; 
+            var params = {screen_name: 'nhuyp87_hw', limit: 20};
+            client.get('statuses/user_timeline', params, function(error, tweets, response) {
             
-            if (!error) {
-                console.log(tweets);
-            }
-        });
-    }
+                var data = [];
+                if (!error) {
+                    for (var i = 0; i < tweets.length; i++) {
+                        data.push([i + 1] + ": " + tweets[i].created_at + ": " + tweets[i].text);
+                    }
+                    console.log(JSON.stringify(data, null, 2));
+                }
+            }); // closes client.get
+        } // closes myTweets 
 
-   }
+        myTweets(); 
+
+   } // closes if statement
     
 // Show artists, song's name, preview link from Spotify and the album that the song is from using spotify-this-sing '<song name here>' 
 
-
-
 if (process.argv[2] == "spotify-this-song") {
 
-        var songName = process.argv[3].toLowerCase().trim();
+        // var songName = process.argv[3].toLowerCase().trim();
 
-        console.log(songName); 
+        // console.log(songName); 
 
-        spotify.search({ type: 'track', query: songName, limit: 1 }, function(err, data) {
-            if ( err ) {
+        spotify.search({ type: 'track', query: process.argv[3]}, function(err, data) {
+            if (err ) {
+
                 console.log('Error occurred: ' + err);
-                return;
+                return;     
+                
             }
+
+                for (var i=0; i<data.length; i++) {
+                    // Do something with 'data'
+                    // console.log(JSON.stringify(data));
+                    console.log(JSON.stringify(data.tracks.items[0].album.artists[0].name));
+                    console.log(JSON.stringify(data.tracks.items[0].album.name));
+                    console.log(JSON.stringify(data.tracks.items[0].name));
+                    console.log(JSON.stringify(data.tracks.items[0].preview_url));
+                    console.log("-----------------------------------------------------------")
+                } // closes for loop
+
+
+
+                
+                
         
-            // Do something with 'data'
-                console.log(); 
+            
                 
 
         });
