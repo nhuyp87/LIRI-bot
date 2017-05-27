a63c474a2c923ec969489e42fbd8a21eef9da7ba
nhuyp <ypham87@gmail.com>
2017-05-27 16:12:39 -0700

    fixed default spotify-this-song search

--- liri.js
+++ liri.js
@@ -26,49 +26,56 @@
             }); // closes client.get
         } // closes myTweets 
 
-        myTweets(); 
+        myTweets();
 
    } // closes if statement
     
 // Show artists, song's name, preview link from Spotify and the album that the song is from using spotify-this-sing '<song name here>' 
 
-if (process.argv[2] == "spotify-this-song") {
+if (process.argv[2] == "spotify-this-song" && process.argv[3]) {
 
-        // var songName = process.argv[3].toLowerCase().trim();
+        var songName = process.argv[3]
 
-        // console.log(songName); 
+        spotify.search({ type: 'track', query: songName}, function(err, data) {
+            if (err ) {
 
-        spotify.search({ type: 'track', query: process.argv[3]}, function(err, data) {
+                console.log('Error occurred: ' + err);
+                return;                     
+            }
+                    // Do something with 'data'
+                    // console.log(JSON.stringify(data));
+                    for (var i=0; i < data.tracks.items.length; i++) {
+                        console.log(JSON.stringify(data.tracks.items[i].album.artists[0].name));
+                        console.log(JSON.stringify(data.tracks.items[i].album.name));
+                        console.log(JSON.stringify(data.tracks.items[i].name));
+                        console.log(JSON.stringify(data.tracks.items[i].preview_url));
+                        console.log("-----------------------------------------------------------");   
+                    };         
+        });
+
+} // closes spotify-this-song if statement
+    // if no song is provided, then program will default to "The Sign" by Ace of Base
+    else if (process.argv[2] == "spotify-this-song" && !process.argv[3]) {
+
+        spotify.search({ type: 'track', query: 'the sign ace of base'}, function(err, data) {
             if (err ) {
 
                 console.log('Error occurred: ' + err);
                 return;     
                 
             }
-
-                for (var i=0; i<data.length; i++) {
                     // Do something with 'data'
                     // console.log(JSON.stringify(data));
-                    console.log(JSON.stringify(data.tracks.items[0].album.artists[0].name));
-                    console.log(JSON.stringify(data.tracks.items[0].album.name));
-                    console.log(JSON.stringify(data.tracks.items[0].name));
-                    console.log(JSON.stringify(data.tracks.items[0].preview_url));
-                    console.log("-----------------------------------------------------------")
-                } // closes for loop
-
-
-
-                
-                
-        
-            
-                
+                    for (var i=0; i < data.tracks.items.length; i++) {
+                        console.log(JSON.stringify(data.tracks.items[i].album.artists[0].name));
+                        console.log(JSON.stringify(data.tracks.items[i].album.name));
+                        console.log(JSON.stringify(data.tracks.items[i].name));
+                        console.log(JSON.stringify(data.tracks.items[i].preview_url));
+                        console.log("-----------------------------------------------------------");   
+                    };   
 
         });
-
-}
-
-    // if no song is provided, then program will default to "The Sign" by Ace of Base
+    }
 
 // Show movie title, movie year, IMDB rating of movie, country of origin, movie language, plot, actors, and Rotten Tomatoes URL using movie-this '<movie name here' command 
 
